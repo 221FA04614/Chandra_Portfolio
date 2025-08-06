8fa04a4ae11d29e5b9943b7abdbae3694d10b2ce
Chandra <221fa04614@gmail.com>
2025-08-06 21:32:00 +0530

    Initial commit for deployment

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000/api/v1/portfolio' });
+const API = axios.create({ baseURL: '/api/v1/portfolio' });
 
 export const fetchProfile = () => API.get('/profile');
 export const fetchProjects = () => API.get('/projects');
